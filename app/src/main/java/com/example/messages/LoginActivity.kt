75d2e94aab87c20c45045df00d21d4b9d3508ef3
Arijit Roy <royarijit1995@gmail.com>
2019-07-21 16:26:21 +0530

    Added Firebase Authentication

--- app/src/main/java/com/example/messages/LoginActivity.kt
+++ app/src/main/java/com/example/messages/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.messages
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -12,14 +13,33 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btn_login.setOnClickListener {
-            val email = email_login.text.toString()
-            val password = password_login.text.toString()
-
-            Toast.makeText(applicationContext, "Attempt login with email/pw: $email/***", Toast.LENGTH_SHORT).show()
+            performSignin()
         }
 
         backToRegister.setOnClickListener{
             finish()
         }
+    }
+
+    private fun performSignin(){
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    // Sign in success, display a message to the user.
+                    Toast.makeText(baseContext, "Successfully created user with uid ${it.result?.user?.uid}",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    return@addOnCompleteListener
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/example/messages/MainActivity.kt
+++ app/src/main/java/com/example/messages/MainActivity.kt
@@ -1,26 +1,56 @@
 package com.example.messages
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_register.setOnClickListener {
-            val email = email_register.text.toString()
-            val password = password_register.text.toString()
-
-            Toast.makeText(applicationContext,"Email : $email Password : $password",Toast.LENGTH_SHORT).show()
+            performRegister()
         }
 
         alreadyHaveAcc.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    private fun performRegister(){
+        val email = email_register.text.toString()
+        val password = password_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()){
+            Toast.makeText(baseContext, "Please enter Email/Password",
+                Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //Firebase Authentication to create a user with email and password
+        auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(baseContext, "Successfully created user with uid ${task.result?.user?.uid}",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    return@addOnCompleteListener
+                }
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 }
